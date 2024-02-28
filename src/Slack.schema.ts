ff0b15a4528085834c340282671be80f7b49ca1c
Stefan Maric <me@stefanmaric.com>
2024-02-28 13:56:33 +0100

    Improve Slack webhook schema to include thread ID and other info
    [f:30m]

--- src/Slack.schema.ts
+++ src/Slack.schema.ts
@@ -7,42 +7,68 @@ export const ChallengeRequest = z.object({
   challenge: z.string(),
 })
 
-export const SubscriptionBase = z.object({
-  api_app_id: z.string(),
-  event_context: z.string(),
-  event_id: z.string(),
-  event_time: z.number(),
-  team_id: z.string(),
-  type: z.string(),
-  token: z.string(),
-})
-
 export const MessageEventBase = z.object({
-  type: z.literal('message'),
-
-  channel: z.string(),
-  hidden: z.boolean().optional(),
-  ts: z.string(),
-  event_ts: z.string(),
   channel_type: z.string(),
+  channel: z.string(),
+  event_ts: z.string(),
+  hidden: z.boolean().optional(),
+  subtype: z.string().optional(),
+  ts: z.string(),
+  type: z.literal('message'),
 })
 
 export const NewMessagePayload = MessageEventBase.extend({
   text: z.string(),
+  thread_ts: z.string().optional(),
   user: z.string(),
 })
 
 export const MessageUpdatePayload = MessageEventBase.extend({
   subtype: z.literal('message_changed'),
+  hidden: z.boolean(),
+  message: z.object({
+    type: z.string(),
+    text: z.string(),
+    user: z.string().optional(),
+    ts: z.string(),
+    edited: z.object({
+      ts: z.string(),
+      user: z.string(),
+    }).optional(),
+    thread_ts: z.string().optional(),
+  }),
+  previous_message: z.object({
+    type: z.string(),
+    text: z.string(),
+    ts: z.string(),
+    user: z.string().optional(),
+    edited: z.object({
+      ts: z.string(),
+      user: z.string(),
+    }).optional(),
+    thread_ts: z.string().optional(),
+  }).optional(),
 })
 
 export const BotMessagePayload = MessageEventBase.extend({
+  app_id: z.string(),
   bot_id: z.string(),
+  subtype: z.literal('bot_message'),
+  username: z.string(),
 })
 
-export const EventCallback = SubscriptionBase.extend({
-  type: z.literal('event_callback'),
+export const EventCallback = z.object({
+  api_app_id: z.string(),
+  context_enterprise_id: z.string().nullable(),
+  context_team_id: z.string(),
+  event_context: z.string(),
+  event_id: z.string(),
+  event_time: z.number(),
   event: z.union([MessageUpdatePayload, NewMessagePayload, BotMessagePayload]),
+  is_ext_shared_channel: z.boolean(),
+  team_id: z.string(),
+  token: z.string(),
+  type: z.literal('event_callback'),
 })
 
 export const SubscriptionEvent = z.discriminatedUnion('type', [ChallengeRequest, EventCallback])
